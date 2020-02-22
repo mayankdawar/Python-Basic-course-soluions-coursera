9ac655b2ed13c4e606f8afdc3ed0511d88109875
Mayank Dawar <mayankdawar29@gmail.com>
2020-02-23 02:20:56 +0530

    Create gradedWeek4(5)_4.py

---
+++ gradedWeek4(5)_4.py
@@ -0,0 +1,7 @@
+#A palindrome is a phrase that, if reversed, would read the exact same.
+#Write code that checks if p_phrase is a palindrome by reversing it and then checking if the reversed version is equal to the original.
+#Assign the reversed version of p_phrase to the variable r_phrase so that we can check your work.
+
+
+p_phrase = "was it a car or a cat I saw"
+r_phrase = p_phrase[::-1]
