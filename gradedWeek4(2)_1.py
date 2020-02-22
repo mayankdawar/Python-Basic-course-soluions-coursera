4adbf6f2d60f08985f3cda2a1db8aa91205c8bae
Mayank Dawar <mayankdawar29@gmail.com>
2020-02-23 00:20:28 +0530

    Create gradedWeek4(2)_1.py

---
+++ gradedWeek4(2)_1.py
@@ -0,0 +1,4 @@
+# Write code to add ‘horseback riding’ to the third position (i.e., right before volleyball) in the list sports.
+
+sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey']
+sports.insert(2,'horseback riding')
