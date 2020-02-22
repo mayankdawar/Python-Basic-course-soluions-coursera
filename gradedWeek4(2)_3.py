ec67c28feed682fe9bfe30273817366c8edb4b40
Mayank Dawar <mayankdawar29@gmail.com>
2020-02-23 00:46:27 +0530

    Create gradedWeek4(2)_3.py

---
+++ gradedWeek4(2)_3.py
@@ -0,0 +1,4 @@
+# Write code to add ‘Guadalajara’ to the end of the list trav_dest using a list method.
+
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'Melbourne']
+trav_dest.append("Guadalajara")
