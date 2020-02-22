2948196679c2bec59f79825f662d83848f8e2a78
Mayank Dawar <mayankdawar29@gmail.com>
2020-02-23 01:23:17 +0530

    Create gradedWeek4(4)_1.py

---
+++ gradedWeek4(4)_1.py
@@ -0,0 +1,6 @@
+# For each character in the string saved in ael, append that character to a list that should be saved in a variable app.
+ael = "python!"
+lst = []
+for i in ael:
+    lst.append(i)
+app = lst
