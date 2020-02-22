6c379557b2a03528722065c5e8786ac7bbc3e93c
Mayank Dawar <mayankdawar29@gmail.com>
2020-02-23 00:47:21 +0530

    Create gradedWeek4(2)_2.py

---
+++ gradedWeek4(2)_2.py
@@ -0,0 +1,5 @@
+# Write code to take ‘London’ out of the list trav_dest.
+
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'London', 'Melbourne']
+trav_dest.remove('London')
+
