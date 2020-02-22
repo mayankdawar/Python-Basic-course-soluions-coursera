ec2a72d001c3806f43a300f462cc25727865b164
Mayank Dawar <mayankdawar29@gmail.com>
2020-02-23 00:50:25 +0530

    Create gradedWeek4(2)_4.py

---
+++ gradedWeek4(2)_4.py
@@ -0,0 +1,3 @@
+# Write code to rearrange the strings in the list winners so that they are in alphabetical order from A to Z.
+winners = ['Kazuo Ishiguro', 'Rainer Weiss', 'Youyou Tu', 'Malala Yousafzai', 'Alice Munro', 'Alvin E. Roth']
+winners.sort()
