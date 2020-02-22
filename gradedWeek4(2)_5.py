f642a883b1f43d464fc44f61061e55635147ef0a
Mayank Dawar <mayankdawar29@gmail.com>
2020-02-23 01:01:01 +0530

    Create gradedWeek4(2)_5.py

---
+++ gradedWeek4(2)_5.py
@@ -0,0 +1,5 @@
+# Write code to switch the order of the winners list so that it is now Z to A. Assign this list to the variable z_winners.
+
+winners = ['Alice Munro', 'Alvin E. Roth', 'Kazuo Ishiguro', 'Malala Yousafzai', 'Rainer Weiss', 'Youyou Tu']
+winners.sort(reverse=True)
+z_winners = winners
