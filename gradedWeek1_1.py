1df892ced2c282e2fc9f284b5f111043eabca3fe
Mayank Dawar <mayankdawar29@gmail.com>
2020-05-07 13:32:33 +0530

    Create gradedWeek1_1.py

---
+++ gradedWeek1_1.py
@@ -0,0 +1,15 @@
+#There is a function we are providing in for you in this problem called square. It takes one integer and returns the square of that integer value. Write code to assign a variable called xyz the value 5*5 (five squared). Use the square function, rather than just multiplying with *.
+
+xyz = 5
+xyz=square(xyz)
+
+
+
+#Write code to assign the number of characters in the string rv to a variable num_chars.
+
+rv = "Once upon a midnight dreary, while I pondered, weak and weary, Over many a quaint and curious volume of forgotten lore, While I nodded, nearly napping, suddenly there came a tapping, As of some one gently rapping, rapping at my chamber door.'Tis some visitor, I muttered, tapping at my chamber door; Only this and nothing more."
+# Write your code here!
+num_chars = len(rv)
+
+
+
